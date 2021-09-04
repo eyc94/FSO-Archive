d19ec305fc8623634e19a73da11496e45d11952e
Eric Chin <ericychin94@gmail.com>
2021-09-04 04:09:08 -0700

    Allowed for arbitrary number of courses and used map method in Content component

--- 02. Communicating With Server/courseinfo/src/components/Content.js
+++ 02. Communicating With Server/courseinfo/src/components/Content.js
@@ -4,10 +4,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <div>
-            <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-            <Part part={props.parts[3].name} exercises={props.parts[3].exercises} />
+            {props.parts.map(part =>
+                <Part key={part.id} part={part.name} exercises={part.exercises} />
+            )}
         </div>
     )
 }
