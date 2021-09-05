be77f9cbe005287b55e50975bddcb4ffc6e4b3d6
Eric Chin <ericychin94@gmail.com>
2021-09-04 20:05:17 -0700

    Added some notes on forms and changed App.js

--- 02. Communicating With Server/part2b/src/App.js
+++ 02. Communicating With Server/part2b/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState(
+        'a new note...'
+    )
+
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
 
 	return (
 		<div>
@@ -11,6 +25,13 @@ const App = ({ notes }) => {
 					<Note key={note.id} note={note} />
 				)}
 			</ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
 		</div>
 	)
 }
