a67a7e2fce1394717a6c328a807b383d5445ae6c
Eric Chin <ericychin94@gmail.com>
2021-09-06 03:46:22 -0700

    Separated components to their own files and imported them to App

---
+++ 02. Communicating With Server/phonebook/src/components/PersonForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PersonForm = (props) => {
+    return (
+        <>
+            <form onSubmit={props.submitHandler}>
+                <div>name: <input value={props.name} onChange={props.nameHandler} /></div>
+                <div>number: <input value={props.number} onChange={props.numberHandler} /></div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default PersonForm
