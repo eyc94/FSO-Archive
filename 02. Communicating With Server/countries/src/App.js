4a8c601122d4bb81cb1988392298116c46c09883
Eric Chin <ericychin94@gmail.com>
2021-09-07 03:38:14 -0700

    Made country filtering and display possible

--- 02. Communicating With Server/countries/src/App.js
+++ 02. Communicating With Server/countries/src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+    const [countryFilter, setCountryFilter] = useState('')
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => {
+                console.log('promise fulfilled')
+                console.log(response.data[0].name)
+                setCountries(response.data)
+            })
+    }, [])
+
+    const changeCountryHandler = (event) => {
+        setCountryFilter(event.target.value)
+    }
+
+    const countriesToShow = countries.filter(country => {
+        return country.name.toLowerCase().includes(countryFilter.toLowerCase())
+    })
+
+    console.log(countriesToShow.map(country => country.name))
+
     return (
         <div>
-            hi
+            <div>find countries <input value={countryFilter} onChange={changeCountryHandler} /></div>
+            <div>
+                {countriesToShow.map(country => 
+                    <div>{country.name}</div>
+                )}
+            </div>
         </div>
     )
 }
