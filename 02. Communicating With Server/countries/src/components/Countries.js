8d18e5c273a4fe7a3c2609e17b0be13320a19401
Eric Chin <ericychin94@gmail.com>
2021-09-07 03:50:53 -0700

    App displays message when list is longer than 10

--- 02. Communicating With Server/countries/src/components/Countries.js
+++ 02. Communicating With Server/countries/src/components/Countries.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Countries = (props) => {
+    if (props.countriesToDisplay.length > 10) {
+        return (
+            <div>
+                Too many matches, specify another filter
+            </div>
+        )
+    }
     return (
         <div>
             {props.countriesToDisplay.map(country => 
