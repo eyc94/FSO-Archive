bbd8d9fde73351464e38783fff6a78fc8c99d864
Eric Chin <ericychin94@gmail.com>
2021-09-07 17:15:57 -0700

    Added sample in comments

--- 02. Communicating With Server/countries/src/components/Countries.js
+++ 02. Communicating With Server/countries/src/components/Countries.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Countries = (props) => {
     if (props.countriesToDisplay.length > 10) {
@@ -35,10 +35,43 @@ const Countries = (props) => {
     return (
         <div>
             {props.countriesToDisplay.map(country => 
-                <div key={country.alpha3Code}>{country.name}</div>
+                <div key={country.alpha3Code}>
+                    {country.name}
+                </div>
             )}
         </div>
     )
 }
 
 export default Countries
+
+
+{/* 
+const [showInfo, setShowInfo] = useState(false)
+
+const handleShowInfo = () => {
+    console.log('clicked')
+    setShowInfo(!showInfo)
+}
+
+
+<button onClick={handleShowInfo}>{showInfo ? 'hide' : 'show'}</button>
+                    
+{showInfo &&
+    <div key={country.alpha3Code}>
+        <h1>{country.name}</h1>
+        <div>Capital: {country.capital}</div>
+        <div>Population: {country.population}</div>
+        <h2>Languages</h2>
+        <ul>
+            {country.languages.map(language =>
+                <li key={language.iso639_2}>{language.name}</li>
+            )}
+        </ul>
+        <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
+            style={{ maxWidth: 200, height: 'auto' }}
+        />
+    </div>
+} */}
