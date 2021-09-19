9ed065926cef95c8658f840a744f6130db764018
Eric Chin <ericychin94@gmail.com>
2021-09-19 01:42:11 -0700

    Added some styling to application

--- 02. Communicating With Server/part2e/src/App.js
+++ 02. Communicating With Server/part2e/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import noteService from './services/notes'
 import Note from './components/Note'
+import './index.css'
 
 const App = () => {
     const [notes, setNotes] = useState([])
