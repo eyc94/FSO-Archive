5f0d1e527c2ca723391012916b9488eb15856bf4
Eric Chin <ericychin94@gmail.com>
2021-09-23 02:42:34 -0700

    Added phonebook from part 2 into part 3

---
+++ 03. Programming a Server With NodeJS and Express/phonebook/src/components/Persons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import DeleteButton from './DeleteButton'
+
+const Persons = (props) => {
+    return (
+        <>
+            {props.displayArray.map(person =>
+                <div key={person.name}>
+                    {person.name} {person.number}
+                    <DeleteButton delPerson={() => props.delPerson(person)} text="delete" />
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Persons
