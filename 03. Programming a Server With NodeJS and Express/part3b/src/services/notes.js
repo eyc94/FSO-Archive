5ef939e893a36fd95d44066df682b0a7fc515a0e
Eric Chin <ericychin94@gmail.com>
2021-09-26 16:12:30 -0700

    Changed baseUrl to relative URL

--- 03. Programming a Server With NodeJS and Express/part3b/src/services/notes.js
+++ 03. Programming a Server With NodeJS and Express/part3b/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://notes-backend-part3a.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
