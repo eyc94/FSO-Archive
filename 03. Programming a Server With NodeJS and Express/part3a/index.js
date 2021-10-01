c2443dc4f478fadf1619e9e8578b463d790eaf06
Eric Chin <ericychin94@gmail.com>
2021-10-01 03:49:07 -0700

    Moved and refactored code for backend

--- 03. Programming a Server With NodeJS and Express/part3a/index.js
+++ 03. Programming a Server With NodeJS and Express/part3a/index.js
@@ -1,40 +1,38 @@
-require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const Note = require('./models/note')
 
-app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
+app.use(express.json())
 
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    },
-]
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
 }
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -68,16 +66,6 @@ app.get('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
-})
-
 app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id)
         .then(result => {
@@ -101,20 +89,15 @@ app.put('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-const PORT = process.env.PORT
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-
-    next(error)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 }
+
+app.use(unknownEndpoint)
 
 app.use(errorHandler)
 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
