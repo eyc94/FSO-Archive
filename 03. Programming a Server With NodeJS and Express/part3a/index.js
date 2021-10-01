709588772a43183c3bab5a22160f05bf5fd8f8c8
Eric Chin <ericychin94@gmail.com>
2021-09-30 18:57:22 -0700

    Use database in more route handlers

--- 03. Programming a Server With NodeJS and Express/part3a/index.js
+++ 03. Programming a Server With NodeJS and Express/part3a/index.js
@@ -39,32 +39,27 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+        date: new Date()
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.get('/', (request, response) => {
