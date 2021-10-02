97742b6e0ef0339dc072d5fca6153bfe692a9323
Eric Chin <ericychin94@gmail.com>
2021-10-02 03:52:44 -0700

    Added error handler for get single person

--- 03. Programming a Server With NodeJS and Express/phonebook-backend/index.js
+++ 03. Programming a Server With NodeJS and Express/phonebook-backend/index.js
@@ -71,9 +71,18 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
 
     // const id = Number(request.params.id)
     // const person = persons.find(person => person.id === id)
