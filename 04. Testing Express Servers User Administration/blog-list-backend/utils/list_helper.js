c7efc433a43c24a6bbc7eaae4d4696bbe808615c
Eric Chin <ericychin94@gmail.com>
2021-10-06 02:21:00 -0700

    Added totalLikes method to list of functions

--- 04. Testing Express Servers User Administration/blog-list-backend/utils/list_helper.js
+++ 04. Testing Express Servers User Administration/blog-list-backend/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const add = (sum, item) => {
+        return sum + item
+    }
+
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce(add, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
