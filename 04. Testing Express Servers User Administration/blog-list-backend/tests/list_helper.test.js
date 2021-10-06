fedad07d4a3bb792153a472c1e1a498959af6acc
Eric Chin <ericychin94@gmail.com>
2021-10-06 03:50:03 -0700

    Removed and moved total likes test to list helper test file

--- 04. Testing Express Servers User Administration/blog-list-backend/tests/list_helper.test.js
+++ 04. Testing Express Servers User Administration/blog-list-backend/tests/list_helper.test.js
@@ -71,3 +71,19 @@ describe('dummy', () => {
     })
 })
 
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        const result = list_helper.totalLikes([])
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = list_helper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = list_helper.totalLikes(blogs)
+        expect(result).toBe(36)
+    })
+})
