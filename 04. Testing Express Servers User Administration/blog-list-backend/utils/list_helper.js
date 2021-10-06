6c06a86fd8875618b6fac9efddb8eb464786d7b6
Eric Chin <ericychin94@gmail.com>
2021-10-06 04:48:25 -0700

    Added mostBlogs function to test

--- 04. Testing Express Servers User Administration/blog-list-backend/utils/list_helper.js
+++ 04. Testing Express Servers User Administration/blog-list-backend/utils/list_helper.js
@@ -33,6 +33,33 @@ const favoriteBlog = (blogs) => {
     return favBlog
 }
 
+const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+
+    if (!authors || authors.length === 0) {
+        return null
+    }
+
+    const countBlogsByAuthor = authors.reduce((acc, curr) => {
+        acc[curr] ? acc[curr]++ : (acc[curr] = 1)
+
+        return acc
+    }, {})
+
+    const authorWithMostBlogsArray = Object.entries(countBlogsByAuthor).reduce((a, b) => (
+        countBlogsByAuthor[a[0]] > countBlogsByAuthor[a[1]]
+            ? a
+            : b
+    ))
+
+    const authorWithMostBlogs = {
+        author: authorWithMostBlogsArray[0],
+        blogs: authorWithMostBlogsArray[1]
+    }
+
+    return authorWithMostBlogs
+}
+
 module.exports = {
     dummy,
     totalLikes,
